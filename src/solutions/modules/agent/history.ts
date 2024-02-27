ce768bd0051f621d6076f793d624ae1420eb2e66
Adam Cowley <adam@adamcowley.co.uk>
2024-02-27 10:01:17 +0000

    remove driver, read() and write() functions

--- src/solutions/modules/agent/history.ts
+++ src/solutions/modules/agent/history.ts
@@ -1,4 +1,4 @@
-import { read, write } from "../../../modules/graph";
+import { initGraph } from "../../../modules/graph";
 
 type UnpersistedChatbotResponse = {
   input: string;
@@ -13,12 +13,14 @@ export type ChatbotResponse = UnpersistedChatbotResponse & {
 
 // tag::clear[]
 export async function clearHistory(sessionId: string): Promise<void> {
-  await write(
+  const graph = await initGraph();
+  await graph.query(
     `
     MATCH (s:Session {id: $sessionId})-[:HAS_RESPONSE]->(r)
     DETACH DELETE r
   `,
-    { sessionId }
+    { sessionId },
+    "WRITE"
   );
 }
 // end::clear[]
@@ -29,7 +31,8 @@ export async function getHistory(
   limit: number = 5
 ): Promise<ChatbotResponse[]> {
   // tag::gettx[]
-  const res = await read<ChatbotResponse>(
+  const graph = await initGraph();
+  const res = await graph.query<ChatbotResponse>(
     `
       MATCH (:Session {id: $sessionId})-[:LAST_RESPONSE]->(last)
       MATCH path = (start)-[:NEXT*0..${limit}]->(last)
@@ -43,12 +46,13 @@ export async function getHistory(
         response.createdAt AS createdAt,
         [ (response)-[:CONTEXT]->(n) | elementId(n) ] AS context
     `,
-    { sessionId }
+    { sessionId },
+    "READ"
   );
   // end::gettx[]
 
   // tag::getreturn[]
-  return res;
+  return res as ChatbotResponse[];
   // end::getreturn[]
 }
 // end::get[]
@@ -76,7 +80,8 @@ export async function saveHistory(
   cypher: string | null = null
 ): Promise<string> {
   // tag::savetx[]
-  const res = await write<{ id: string }>(
+  const graph = await initGraph();
+  const res = await graph.query<{ id: string }>(
     `
     MERGE (session:Session { id: $sessionId }) // <1>
 
@@ -125,12 +130,21 @@ export async function saveHistory(
 
     RETURN DISTINCT response.id AS id
   `,
-    { sessionId, source, input, output, rephrasedQuestion, cypher: cypher, ids }
+    {
+      sessionId,
+      source,
+      input,
+      output,
+      rephrasedQuestion,
+      cypher: cypher,
+      ids,
+    },
+    "WRITE"
   );
   // end::savetx[]
 
   // tag::savereturn[]
-  return res[0].id;
+  return res && res.length ? res[0].id : "";
   // end::savereturn[]
 }
 // end::save[]
