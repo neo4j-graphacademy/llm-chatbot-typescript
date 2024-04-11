0ea1c1ae9ced81fcb60197b4e63f0c7d5378217c
chaitanya vardhan kandula <Cvkandula1729@gmail.com>
2024-04-11 23:39:27 +0000

    commiting changes

--- src/modules/agent/history.ts
+++ src/modules/agent/history.ts
@@ -35,6 +35,25 @@ export async function getHistory(
   // const graph = await initGraph()
   // const res = await graph.query<ChatbotResponse>(cypher, { sessionId }, "READ")
   // return res
+const graph = await initGraph();
+const res = await graph.query<ChatbotResponse>(
+  `
+    MATCH (:Session {id: $sessionId})-[:LAST_RESPONSE]->(last)
+    MATCH path = (start)-[:NEXT*0..${limit}]->(last)
+    WHERE length(path) = 5 OR NOT EXISTS { ()-[:NEXT]->(start) }
+    UNWIND nodes(path) AS response
+    RETURN response.id AS id,
+      response.input AS input,
+      response.rephrasedQuestion AS rephrasedQuestion,
+      response.output AS output,
+      response.cypher AS cypher,
+      response.createdAt AS createdAt,
+      [ (response)-[:CONTEXT]->(n) | elementId(n) ] AS context
+  `,
+  { sessionId },
+  "READ"
+);
+return res as ChatbotResponse[];
 }
 // end::get[]
 
@@ -64,5 +83,68 @@ export async function saveHistory(
   // const graph = await initGraph()
   // const res = await graph.query<{id: string}>(cypher, params, "WRITE")
   // return res[0].id
+
+const graph = await initGraph();
+const res = await graph.query<{ id: string }>(
+  `
+  MERGE (session:Session { id: $sessionId }) // (1)
+
+  // <2> Create new response
+  CREATE (response:Response {
+    id: randomUuid(),
+    createdAt: datetime(),
+    source: $source,
+    input: $input,
+    output: $output,
+    rephrasedQuestion: $rephrasedQuestion,
+    cypher: $cypher,
+    ids: $ids
+  })
+  CREATE (session)-[:HAS_RESPONSE]->(response)
+
+  WITH session, response
+
+  CALL {
+  WITH session, response
+
+    // <3> Remove existing :LAST_RESPONSE relationship if it exists
+    MATCH (session)-[lrel:LAST_RESPONSE]->(last)
+    DELETE lrel
+
+    // <4? Create :NEXT relationship
+    CREATE (last)-[:NEXT]->(response)
+  }
+
+
+  // <5> Create new :LAST_RESPONSE relationship
+  CREATE (session)-[:LAST_RESPONSE]->(response)
+
+  // <6> Create relationship to context nodes
+  WITH response
+
+  CALL {
+    WITH response
+    UNWIND $ids AS id
+    MATCH (context)
+    WHERE elementId(context) = id
+    CREATE (response)-[:CONTEXT]->(context)
+
+    RETURN count(*) AS count
+  }
+
+  RETURN DISTINCT response.id AS id
+`,
+  {
+    sessionId,
+    source,
+    input,
+    output,
+    rephrasedQuestion,
+    cypher: cypher,
+    ids,
+  },
+  "WRITE"
+);
+return res && res.length ? res[0].id : "";
 }
 // end::save[]
--- src/modules/graph.ts
+++ src/modules/graph.ts
@@ -13,9 +13,13 @@ let graph: Neo4jGraph;
  */
 export async function initGraph(): Promise<Neo4jGraph> {
   if (!graph) {
-    // TODO: Create singleton and wait for connection to be verified
+    graph = await Neo4jGraph.initialize({
+      url: process.env.NEO4J_URI as string,
+      username: process.env.NEO4J_USERNAME as string,
+      password: process.env.NEO4J_PASSWORD as string,
+      database: process.env.NEO4J_DATABASE as string | undefined,
+    });
   }
-
   return graph;
 }
 // end::graph[]
